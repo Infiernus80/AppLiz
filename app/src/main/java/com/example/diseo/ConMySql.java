013a4a03886ebd2106c186f3c2b3f579b38ca556
Infiernus80 <e.jonathansandoval200106@gmail.com>
2020-07-02 19:21:42 -0500

    Conexion MySql

--- app/src/main/java/com/example/diseo/ConMySql.java
+++ app/src/main/java/com/example/diseo/ConMySql.java
@@ -8,7 +8,7 @@ import java.sql.DriverManager;
 public class ConMySql {
     //10.0.2.2 ip para trabajar de manera local con android
     private String ip="192.168.1.139:3307"; // Ip de mi mi maquina o del servidor
-    private String bd="empresa";
+    private String bd="liz";
     private String usuarioBD="android";
     private String passBD="android";
     private String url="jdbc:mysql://" + ip + "/"+ bd;
--- app/src/main/java/com/example/diseo/MainActivity.java
+++ app/src/main/java/com/example/diseo/MainActivity.java
@@ -3,37 +3,110 @@ package com.example.diseo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputContentInfo;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class MainActivity extends AppCompatActivity {
     private EditText Correo, Contraseña;
     private Button Iniciar;
+    ProgressBar barInicio;
+    ConMySql conexion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Correo = (EditText) findViewById(R.id.Correo);
-        Contraseña = (EditText) findViewById(R.id.Contraseña);
-        Iniciar = (Button) findViewById(R.id.btn_Iniciar);
+        Correo =  findViewById(R.id.Correo);
+        Contraseña =  findViewById(R.id.Contraseña);
+        Iniciar =   findViewById(R.id.btn_Iniciar);
+        barInicio = findViewById(R.id.barInicio);
+
+        barInicio.setVisibility(View.GONE);
+
+        conexion = new ConMySql();
 
         Iniciar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (Correo.getText().toString().equals("Bodega") && Contraseña.getText().toString().equals("Root")) {
-                    Intent intent = new Intent(v.getContext(), MenuPrincipalB.class);
-                    startActivity(intent);
-
-                }
-
-
+                inicioSesion sesion = new inicioSesion();
+                sesion.execute("");
             }
-        });
+        });//Termina onClick iniciar
+    }//Termina onCreate
+    public class inicioSesion extends AsyncTask<String, String, String> {
+        String mensaje = "";
+        boolean exito = false;
+        String usuario = Correo.getText().toString();
+        String pass = Contraseña.getText().toString();
 
-    }
-}
+
+        @Override
+        protected void onPreExecute() {
+            barInicio.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected void onPostExecute(String msj) {
+
+            barInicio.setVisibility(View.GONE);
+            Toast.makeText(MainActivity.this, msj, Toast.LENGTH_LONG).show();
+
+            if (exito) {
+                Intent iniciar = new Intent(getApplicationContext(), MenuPrincipalB.class);
+                startActivity(iniciar);
+            }
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            Connection con = conexion.Conectar();
+            //Preparamos query a la base de datos
+            String query = "select * from empleados where Correo=? and Contrasenia=?";
+            if (con != null) {
+                try {
+
+                    PreparedStatement ps = con.prepareStatement(query);
+                    ps.setString(1, usuario);
+                    ps.setString(2, pass);
+
+                    //ejecutar la instruccion (query) y prepara la respuesta de la base de datos
+                    ResultSet rs = ps.executeQuery();
+
+                    if (rs.next()) {
+                        exito = true;
+                        mensaje = "Bienvenido: " + rs.getString("nombre");
+                    } else {
+                        mensaje = "Usuario o contraseña incorrectos";
+                    }
+
+
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+                //Cierre de sesión
+                try {
+                    con.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                mensaje = "Error al conectar a la base de datos";
+            }
+
+
+            return mensaje;
+        }
+    }//Termina metodo Iniciar Sesion
+}//Termina class ManinActivity
